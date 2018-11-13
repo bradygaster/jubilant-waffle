78eaf328458be3ad010c3ab46208582692695611
Brady Gaster <bradyg@microsoft.com>
2018-11-13 13:04:48 -0800

    added web api

--- src/Places.Api/Controllers/PlacesController.cs
+++ src/Places.Api/Controllers/PlacesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace api.Controllers
+namespace Places.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -19,27 +19,9 @@ namespace api.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<string> Get(Guid id)
         {
             return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
