b02abce5d28ef871a0ab44bb6cfaf645793f248a
Brady Gaster <bradyg@microsoft.com>
2018-11-14 16:52:32 -0800

    wired up messages to admin and mobile web apps

--- src/DispatchR.Api/Hubs/DispatchRHub.cs
+++ src/DispatchR.Api/Hubs/DispatchRHub.cs
@@ -1,10 +1,25 @@
 
+using System;
+using System.Linq;
+using DispatchR.Data.Models;
+using DispatchR.Api.Models;
 using Microsoft.AspNetCore.SignalR;
 
 namespace DispatchR.Hubs
 {
     public class DispatchRHub : Hub
     {
-        
+        public DispatchRHub(PlacesContext context)
+        {
+            PlacesContext = context;
+        }
+
+        private PlacesContext PlacesContext { get; }
+
+        public void RequestAssistance(Guid placeId)
+        {
+            var place = PlacesContext.Places.First(x => x.Id == placeId).CreateViewModel();
+            Clients.Others.SendCoreAsync("assistanceRequested", new object[] { place });
+        }
     }
 }
